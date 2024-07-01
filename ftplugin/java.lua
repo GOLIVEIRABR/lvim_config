6f09c9132472994088e219d4f9793c2cd6700d89
Geazi Oliveira <geazioliveira@hotmail.com>
2024-07-01 08:56:57 -0300

    backup lvim config

---
+++ ftplugin/java.lua
@@ -0,0 +1,163 @@
+local status, jdtls = pcall(require, "jdtls")
+if not status then
+  return
+end
+
+-- Setup Workspace
+local home = os.getenv "HOME"
+local workspace_path = home .. "/.local/share/lunarvim/jdtls-workspace/"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
+-- Determine OS
+local os_config = "linux"
+if vim.fn.has "mac" == 1 then
+  os_config = "mac"
+end
+
+-- Setup Capabilities
+local capabilities = require("lvim.lsp").common_capabilities()
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
+-- Setup Testing and Debugging
+local bundles = {}
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar"), "\n"))
+vim.list_extend(
+  bundles,
+  vim.split(
+    vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
+    "\n"
+  )
+)
+
+lvim.builtin.dap.active = true
+local config = {
+  cmd = {
+    "java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xms1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    "-javaagent:" .. home .. "/.local/share/lvim/mason/packages/jdtls/lombok.jar",
+    "-jar",
+    vim.fn.glob(home .. "/.local/share/lvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    "-configuration",
+    home .. "/.local/share/lvim/mason/packages/jdtls/config_" .. os_config,
+    "-data",
+    workspace_dir,
+  },
+  root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew"  },
+  capabilities = capabilities,
+
+  settings = {
+    java = {
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-1.8",
+            path = "/usr/lib/jvm/java-8-openjdk-amd64/jre",
+          },
+          {
+            name = "JavaSE-11",
+            path = "/usr/lib/jvm/java-11-openjdk-amd64",
+          },
+          {
+            name = "JavaSE-17",
+            path = "/usr/lib/jvm/java-17-openjdk-amd64",
+          },
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk-amd64",
+          },
+        },
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all", -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+    signatureHelp = { enabled = true },
+    extendedClientCapabilities = extendedClientCapabilities,
+  },
+  init_options = {
+    bundles = bundles,
+  },
+}
+
+config["common_on_attach"] = function(client, bufnr)
+  local _, _ = pcall(vim.lsp.codelens.refresh)
+	require("jdtls").setup_dap({ hotcodereplace = "auto" })
+	require("lvim.lsp").on_attach(client, bufnr)
+  local status_ok, jdtls_dap = pcall(require, "jdtls.dap")
+  if status_ok then
+    jdtls_dap.setup_dap_main_class_configs()
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+  end,
+})
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "google_java_format", filetypes = { "java" } },
+}
+
+require("jdtls").start_or_attach(config)
+
+local dap = require("dap")
+
+dap.adapters.java = function(callback, config)
+  callback({
+    type = 'server',
+    host = '0.0.0.0',  -- ou o IP da máquina remota
+    port = '5005'
+  })
+end
+
+dap.configurations.java = {
+  {
+    type = "java",
+    request = "attach",
+    name = "Debug (Remote)",
+    hostName = "0.0.0.0",  -- ou o IP da máquina remota
+    port = 5005,
+  },
+}
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
